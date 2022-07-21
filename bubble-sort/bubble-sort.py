86891da09c3a1fc97f4afd65fe5694a7263e5ebc
mori-dv <morimori.ph@gmail.com>
2022-07-21 17:48:43 +0430

    add bubble sort algorithm

---
+++ bubble-sort/bubble-sort.py
@@ -0,0 +1,8 @@
+def bubble_sort(array):
+    for i in range(len(array)):
+        for j in range(i, len(array)):
+            if array[i] > array[j]:
+                array[i], array[j] = array[j], array[i]
+    return array
+
+
