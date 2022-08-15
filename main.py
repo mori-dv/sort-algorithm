68c98736bab35b7ed7cd7e8d2d0ec450f2fd80e8
mori-dv <morimori.ph@gmail.com>
2022-08-15 20:54:59 +0430

    add two pointers algorithm to main script

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from bubble_sort.bubble_sort import bubble_sort
 from insertion_sort.insertion_sort import insertion_sort
 from quick_sort.quick_sort import quick_sort
 from selection_sort.selection_sort import selection_sort
+from two_pointers.two_pointers import two_pointers
 
 
 if __name__ == "__main__":
@@ -15,11 +16,12 @@ please Choose one of the algorithm you want:
 [2] insertion sort
 [3] quick sort
 [4] selection sort
-[5] exit"""
+[5] two_pointers
+[6] exit"""
 )
         algorithm = input().strip()
 
-        if algorithm == '5':
+        if algorithm == '6':
             break
 
         print("Please give me the array you want to sort which every element should be seperated by space: ")
@@ -49,6 +51,11 @@ please Choose one of the algorithm you want:
             print("++++++++++++++++++ SORTED ARRAY ++++++++++++++++++++++")
             print(selection_sort(array))
 
+        elif algorithm == '5':
+            print("++++++++++++++++++ NOT SORTED ARRAY ++++++++++++++++++")
+            print(array)
+            print("++++++++++++++++++ SORTED ARRAY ++++++++++++++++++++++")
+            print(two_pointers(array))
     print("Thanks for using me! \nGoodBye =)")
 
 
